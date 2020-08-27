e8ffbb3d95e8a3b7e0f3779b838df004ab18bde2
izharabbasi <izharabbasi103@gmail.com>
2020-08-28 00:16:33 +0500

    first project

--- project1.py
+++ project1.py
@@ -13,15 +13,14 @@ time.sleep(3)
 
 first_name = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[1]/div/input").send_keys("izhar")
 last_name = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[2]/div/input").send_keys("abbasi")
-User_name = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[3]/div/input").send_keys('izharabbasi')
-Email = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[4]/div/input").send_keys('izhar_abbasi.1@hotmail.com')
+User_name = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[3]/div/input").send_keys('izharabbasadiii')
+Email = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[4]/div/input").send_keys('izhar_abbasadi102@hotmail.com')
 Password = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[5]/div/input").send_keys('12345678')
 Password_confirmation = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[6]/div/input").send_keys('12345678')
 
 Register = driver.find_element_by_xpath("//button[@value='register']").click()
 
-time.sleep(4)
+time.sleep(10)
 
 driver.close()
 
-#(//div[@class='tutor-form-col-6'])[1]
