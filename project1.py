01c6a64fe6df86485108c0e9deff79c695056ff7
izharabbasi <izharabbasi103@gmail.com>
2020-08-27 23:13:14 +0500

    complete project 1

--- project1.py
+++ project1.py
@@ -4,7 +4,24 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("https://rifinder.com/")
-
 time.sleep(3)
 
+sign_up = driver.find_element_by_xpath("(//span[@class='elementor-button-text'])[1]").click()
+time.sleep(2)
+create_account = driver.find_element_by_xpath("(//p[@class='tutor-form-register-wrap'])[1]/a").click()
+time.sleep(3)
+
+first_name = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[1]/div/input").send_keys("izhar")
+last_name = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[2]/div/input").send_keys("abbasi")
+User_name = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[3]/div/input").send_keys('izharabbasi')
+Email = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[4]/div/input").send_keys('izhar_abbasi.1@hotmail.com')
+Password = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[5]/div/input").send_keys('12345678')
+Password_confirmation = driver.find_element_by_xpath("(//div[@class='tutor-form-col-6'])[6]/div/input").send_keys('12345678')
+
+Register = driver.find_element_by_xpath("//button[@value='register']").click()
+
+time.sleep(4)
+
 driver.close()
+
+#(//div[@class='tutor-form-col-6'])[1]
